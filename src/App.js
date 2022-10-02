b596da2d923d9eb44ce702669dbe2693ad742275
layssz <104585130+layssz@users.noreply.github.com>
2022-10-02 13:54:09 -0300

    sajsj

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Frase from './components/Frase';
+
+import Cards from './components/Cards';
+
+
 
 function App() {
 
   
   return (
     <div className="App">
-     <h1> Olá estou começanco a programar no React :v</h1>
-     <img src={logo} className="App-logo" alt="logo" />
-     <SayMyName nome="lays"/>
-     <Frase/>
-     <HelloWorld/>
-    </div>
+     <h1> Memory Game - React</h1>
+     <Cards/>
+
+     
+     
+    
+  </div>
   );
 }
 
